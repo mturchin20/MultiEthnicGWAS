5fb6dae8dd4546fae9642161fccbc21855fc1611
Michael Turchin <mturchin20@gmail.com>
2019-12-20 14:40:40 -0500

    Updating MultiEthnicGWAS.Workbook.vs1.sh -- UKB full dataset rerun (code build-up: Brit.4k/.10k replicate run build-up)

--- MultiEthnicGWAS.Workbook.vs1.sh
+++ MultiEthnicGWAS.Workbook.vs1.sh
@@ -430,7 +430,7 @@ cat /users/mturchin/data/ukbiobank_jun17/mturchin/ukb9200.2017_8_WinterRetreat.P
 
 #20191119 NOTE -- yIntrcptFix (include '- 1'), BMIage (use age-adjusted BMI for Waist/Hip adjustment) 
 #From https://stats.idre.ucla.edu/r/modules/coding-for-categorical-variables-in-regression-models/, https://stats.idre.ucla.edu/r/modules/coding-for-categorical-variables-in-regression-models/
-for j in `cat <(echo $UKBioBankPopsRnd2 | perl -lane 'print join("\n", @F);') | head -n 8 | tail -n 8`; do
+for j in `cat <(echo $UKBioBankPopsRnd2 | perl -lane 'print join("\n", @F);') | tail -n 8 | head -n 8 | tail -n 8`; do
         ancestry1=`echo $j | perl -ane 'my @vals1 = split(/;/, $F[0]); print $vals1[0];'`
         ancestry2=`echo $j | perl -ane 'my @vals1 = split(/;/, $F[0]); print $vals1[1];'`
         echo $ancestry1 $ancestry2
